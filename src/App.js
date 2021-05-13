be56d9c6768176a08c7f8866de88b5a25fe730de
harinath1314 <harinath1314@msitprogram.net>
2021-05-13 06:32:08 +0530

    props session

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './component/MainPage';
+import FunctionalComponent from './Components/FunctionalComponent/FunctionalComponent';
 
 
 function App() {
   return (
     <div className="App">
+       
       <MainPage></MainPage>
+      
     </div>
   );
 }
---
+++ src/Components/FunctionalComponent/FunctionalComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function FunctionalComponent(props) {
+    var x = "harinath Reddy";
+    var age = 26;
+    var place = "hyderabad";
+
+    return (
+        <div>
+            <p>
+                name :- {x}
+            </p>
+            <p>{props.name.name}</p>
+            <p> age :-  {props.name.age}</p>
+            <p> place of living:- {props.name.place}</p>
+        </div>
+    )
+}
